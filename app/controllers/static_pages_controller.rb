681c37aeb17ebb6abc2c74ad48a7f8606196ed6a
STRiGA <hsq.plus@gmail.com>
2020-09-26 11:43:14 +0000

    Complete beta

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,25 @@
 class StaticPagesController < ApplicationController
   def home
+    @menu0 = "selected"
+    @menu1 = "unselected"
+    @menu2 = "unselected"
   end
 
-  def help
+  def ranking
+    @menu0 = "unselected"
+    @menu1 = "selected"
+    @menu2 = "unselected"
+  end
+  
+  def setting
+    @menu0 = "unselected"
+    @menu1 = "unselected"
+    @menu2 = "selected"
+  end
+  
+  def signin
+    @menu0 = "unselected"
+    @menu1 = "unselected"
+    @menu2 = "unselected"
   end
 end
